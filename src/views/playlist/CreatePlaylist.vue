865ae9f3e0467dd033b3e97cbf165920d44980d5
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-27 01:34:55 +0200

    filename and some styling changes

--- src/views/playlist/CreatePlaylist.vue
+++ src/views/playlist/CreatePlaylist.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-50 p-8 rounded-lg shadow-lg">
     <h4 class="text-center text-xl font-medium mb-3">Create New Playlist</h4>
-    <form @submit.prevent="handleSubmit" class="bg-gray-200 p-2 rounded-lg">
+    <form @submit.prevent="handleSubmit" class="bg-gray-200 p-2 rounded-lg shadow-lg">
       <input
         v-model="title"
         type="text"
@@ -12,20 +12,24 @@
       <textarea
         v-model="description"
         class="block w-full p-2 my-2 rounded-lg"
+        rows="5"
         placeholder="Playlist Description..."
         required
       ></textarea>
+      <!-- custom Label design -->
+        <input @change="showFileName" type="file" class="hidden" id="fileUpload" />
       <label
-        class="inline-block my-5 bg-gray-300 text-gray-700 border-2 border-dashed border-gray-700 p-2 rounded-lg"
+      for="fileUpload"
+        class="inline-block my-5 bg-gray-300 text-gray-700 border-2 border-dashed border-gray-700 p-2 rounded-lg cursor-pointer"
       >
-        Click here to upload playlist cover
-        <input type="file" class="sr-only" />
+        Upload playlist cover
       </label>
+      <span class="mx-2 text-yellow-600">{{ fileName ? fileName : "No file choosen..." }}</span>
       <!-- errors -->
       <div class="text-red-500"></div>
       <!-- Save button -->
       <button
-        class="block mx-auto bg-red-500 hover:bg-gray-200 py-2 px-5 rounded-lg text-gray-100 hover:text-red-500 shadow-md transition-all duration-200"
+        class="block mx-auto mt-4 mb-4 bg-red-500 hover:bg-gray-200 py-2 px-5 rounded-lg text-gray-100 hover:text-red-500 shadow-md transition-all duration-200"
       >Create</button>
     </form>
   </div>
@@ -37,12 +41,17 @@ export default {
   setup() {
     const title = ref('')
     const description = ref('')
+    const fileName = ref('')
 
     const handleSubmit = () => {
       console.log(title.value, description.value);
     }
 
-    return { title, description, handleSubmit }
+    const showFileName =(e) => {
+      fileName.value = e.target.files[0].name
+    }
+
+    return { title, description, handleSubmit, showFileName, fileName }
   }
 };
 </script>
